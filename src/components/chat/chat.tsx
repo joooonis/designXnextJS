fe349b7e726c5bd2754a6285f3f375e58813a087
jooooonis <xowns3213@naver.com>
2023-05-15 16:49:15 +0900

    ✨ feat : chatting animation

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,4 +1,23 @@
+import anime from 'animejs';
+import { useEffect, useState } from 'react';
+import Wait from './wait';
+
 export default function Chat({ isRight = false }) {
+  const [isChatLoad, setIsChatLoad] = useState(false);
+  useEffect(() => {
+    anime({
+      targets: '.message',
+      opacity: [0, 1],
+      easing: 'easeInOutQuad',
+      duration: 2000,
+    });
+
+    if (isChatLoad) return;
+    setTimeout(() => {
+      setIsChatLoad(true);
+    }, 5000);
+  }, [isChatLoad]);
+
   return (
     <div
       className={`w-full flex space-x-4 ${
@@ -6,8 +25,12 @@ export default function Chat({ isRight = false }) {
       }`}
     >
       {!isRight && <div className="rounded-full bg-gray-50 w-16 h-16"></div>}
-      <div className="rounded-full text-slate-800 opacity-80 font-semibold w-fit flex justify-center items-center px-8 py-2 bg-gray-50 h-16">
-        여긴 어디지?
+      <div className="rounded-full text-slate-600 font-semibold w-fit flex justify-center items-center px-8 py-2 bg-gray-50 h-16">
+        {isChatLoad ? (
+          <div className="message opacity-0">여긴 어디지?</div>
+        ) : (
+          <Wait />
+        )}
       </div>
       {isRight && <div className="rounded-full bg-gray-50 w-16 h-16"></div>}{' '}
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,22 @@ module.exports = {
       fontFamily: {
         Balsamiq: ['Balsamiq Sans', 'rsive'], // 영문 폰트
       },
+      animation: {
+        bounce1: 'bounce 2s ease-in-out infinite ',
+        bounce2: 'bounce 2s 0.5s ease-in-out infinite ',
+        bounce3: 'bounce 2s 1s ease-in-out infinite ',
+      },
+      keyframes: {
+        bounce: {
+          '0%, 20%': { trasnsform: `translate(0,0)` },
+          '50%': {
+            transform: 'translateY(-150%)',
+          },
+          '85%, 100%': {
+            trasnsform: `translate(0,0)`,
+          },
+        },
+      },
     },
   },
 
